9052095eb5a8669368666ba224fc3cde1ddf72e9
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-09 21:35:54 +0300

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,56 @@
+node 'slave1' {
+  include static_conf
+}
+node 'slave2' {
+  include dynamic_conf
+}
+
+
+class dynamic_conf {
+  $package = ['httpd','php']
+  package {$package: ensure => 'installed'}
+  file {'/var/www/dynamic/html':
+    ensure => directory
+    }
+  file {'/var/www/dynamic/html':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+    }
+  file {'/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/httpd.conf'
+   }
+  file {'/etc/httpd/conf.d/dynamic.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/dynamic.conf',
+    notify => Service['httpd']
+   }
+  service {'httpd':
+    ensure => running,
+    enable => true
+   }
+}
+
+class static_conf {
+  $package = ['httpd','php']
+  package {$package: ensure => 'installed'} 
+  file {'/var/www/static/html':
+    ensure => directory
+   }
+  file {'/var/www/static/html':
+    ensure => file,
+    source => 'puppet:///modules/static/index.html'
+   }
+  file {'/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    source => 'puppet:///modules/static/httpd.conf'
+   }
+  file {'/etc/httpd/conf.d/static.conf':
+    ensure => file,
+    source => 'puppet:///modules/static/static.conf'
+   }
+  service {'httpd':
+    ensure => running, 
+    enable => true
+  }  
+}
