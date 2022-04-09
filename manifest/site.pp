bcbca5c780c3498bb6ef3e79ea319a35879cfe54
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-09 14:55:13 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,8 +1,24 @@
 node 'slave1', 'slave2' {
   $package = ['httpd','php']
   package {$package: ensure => 'installed'}
-    service { 'httpd':
-    ensure =>running,
-    enable => true,
- }
+}
+node slave2 {
+  file {'/var/www/dynamic/html':
+    ensure => directory
+  }
+  file {'/var/www/dynamic/html':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+  }
+  file{'/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/httpd.conf',
+  file{'/etc/httpd/conf.d/dynamic.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/dynamic.conf',
+    notify => Service['httpd']
+  }
+  service{'httpd':
+    ensure => running
+  }
 }
