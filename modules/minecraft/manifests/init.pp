48d2b321fd36b1e37a64b46fb9b16b0f89dac6f8
ddd015 <101343055+ddd015@users.noreply.github.com>
2022-04-10 20:45:56 +0300

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -1,3 +1,20 @@
 class minecraft {
+  package {'java-17-openjdk-headless':
+    ensure => installed
+    }
+ file {'/opt/minecraft':
+   ensure => directory
+   }
+ include wget
+ wget::fetch { "download minecraft":
+      source      => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+      destination => '/opt/minecraft',
+      timeout     => 0,
+      verbose     => false
+    }
+ file {'/opt/minecraft/eula.txt':
+   ensure => file,
+   content => 'eula=true'
+   }
   
 }
